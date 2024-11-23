aa1bfde353ec59d5dc5902f3fa3f73e43d58353f
pingzhihe <pzh1760473545@gmail.com>
2024-11-23 15:38:25 +1100

    rewrite base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://pingzhihe.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/myBlog/',
   organizationName: 'pingzhihe',
   projectName: 'Charles',
   trailingSlash: false,
