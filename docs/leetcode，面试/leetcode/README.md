f5f0aeaf3c5d69418659bacaff2b894a2fd8780c
pingzhihe <pzh1760473545@gmail.com>
2024-12-05 23:54:18 +0800

    Add the Respack plugin

---
+++ docs/leetcode，面试/leetcode/README.md
@@ -0,0 +1,4 @@
+# Leetcode 算法
+## 网上资源汇总
+[labuladong 的算法笔记](https://labuladong.online/algo/)
+
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,11 +20,17 @@ const config: Config = {
   trailingSlash: false,
 
 
+  
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  // The Faster plugin 
+  future: {
+    experimental_faster: true,
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
